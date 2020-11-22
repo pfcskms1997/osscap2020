462c4053fc2b293455efee2ed036de3066aca358
Minseong Kang <pfcskms1997@gmail.com>
2020-11-22 16:01:06 +0900

    arrangement repo

--- source_code/catchmind.py
+++ source_code/catchmind.py
@@ -1,9 +1,23 @@
-#공룡게임으로 얻은 색깔 블럭 기록 받아오기
-#list로 
+#아직 해결 못한거:LED 출력, 음성인식 
+
+
+#공룡게임으로 얻은 색깔 블럭 갯수를 colorlistcnt
+#색(빨주노초파보흰) colorlist
+#colorlist=["red","orange","yellow","green","blue","purple","gray"]
+colorlist=["black","red","green","yellow","blue","purple","skyblue"]
+#colorlistcnt=[0,2,3,0,4,2,3]
+colorlistcnt=[1,2,3,1,4,2,3]
 
 from turtle import*
 import time
+import led_display as led
+import threading
 
+def LED_init():
+    t=threading.Thread(target=led.main, args=())
+    t.setDaemon(True)
+    t.start()
+    return
 
 #시작시간 측정
 start=time.time()
@@ -11,7 +25,7 @@ start=time.time()
 pencolor("black")
 title("Catch my drawing")
 
-#화면 크기
+#화면 설정
 setup(1600,800)
 hideturtle()
 speed(100000)
@@ -36,61 +50,49 @@ for i in range(31):
     backward(800)
     v+=50
     
-
-
-#
-def drawShape(x,y):    
-    pensize(50)
+#색깔 별로 화면에 색칠해주기
+def drawColor(color,b):
+    pensize(30)
+    pencolor(color)
     up()
-    goto(x,y)
+    goto(725,b)
     down()
-    goto(x,y-25)
-    onscreenclick(None)    
+    goto(725,b-15)
+
+
+#화면에 색깔의 존재 나타내기
+for i in range(0,7,1):
+    if colorlistcnt[i]>0:
+        drawColor(colorlist[i],335-i*50)
+
+#프로그램(창) 종료
+def endP():
+    bye()
+    print("걸린 시간:",time.time()-start)
     
-
-#
-
-
-        
-#색칠하기    
-
-#시작지점 50 나눈거에서 나머지빼줘야함
+#클릭에 따라 색칠하기
+def drawShape(x, y):    
+    ledcolor = 2 
+    if 700<=x<=750:
+        for k in range(0,7,1):
+            if 300-50*k<y<=350-50*k:
+                if colorlistcnt[k]>0:
+                    pencolor(colorlist[k])
+                    ledcolor = k
+    a=x-x%50+25    
+    b=(y//50+1)*50
+    up()
+    goto(a,b-15)
+    down()
+    goto(ia,b-30)
+    led.set_pixel(int((a+775)/50), int((400-b)/50), ledcolor)
     
-while(1):
-    color=input("사용할 색을 입력하시오.(종료를 원하면 a/ 취소를 원하면 z)")
-    if color =="a":
-        break
- 
-            #elif color=="z":
-        #취소에 대한 내용
-    elif color=='red':
-        pencolor("red")
-        onscreenclick(drawShape)
-        mainloop()
-
-    elif color=='green':
-        pencolor("green")
-        onscreenclick(drawShape)
-        mainloop()
+    onkey(endP,"space")
+    listen()
+    
+LED_init()
+while True:
+    onscreenclick(drawShape)
+    mainloop()
         
-    else:
-            #if list[1]==0: print("green 색깔 블럭이 없습니다.")
-            #else 진행
-        print("잘못입력하셨습니다.")
-            #LED matrix에도 위치에 따른 색 출력
-        
-
-#색 정하고 클릭한 칸 fill
-        
-        #onscreenclick:화면내의 특정지점을 클릭하면 그 좌표를 기억하고 실행하는 함수
-        #화면 클릭 시 함수 실행
-        # 실행되는 함수는 현재 좌표(클릭 지점)인 (x,y)를 매개변수로 이용
-
-
 #음성인식해서 정답일 시
-        #종료시간 측정
-        #종료시간 - 시작시간 보여줌
-        #종료시간 - 시작시간 기록(?)
-
-print("걸린 시간:",time.time()-start)
-
