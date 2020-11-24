a31d0d2eddf5a57dddb45972812ef8fda87b20db
pfcskms1997 <62384846+pfcskms1997@users.noreply.github.com>
2020-11-24 10:28:14 +0900

    Update Collision_uadate.py

--- Collision_uadate.py
+++ Collision_uadate.py
@@ -1,4 +1,6 @@
 import pygame
+import led_display as led
+import threading
 from random import *
 
 pygame.init()
@@ -25,25 +27,33 @@ Color_Set = [BLACK, RED, GREEN, YELLOW, BLUE, PURPLE, SKYBLUE, WHITE]
 speed = 0.4
 run = True
 dino_array = [ [ 1, 0, 0, 0, 1, 0 ],
-                [ 1, 0, 0, 1, 1, 1 ], 
-                [ 1, 1, 1, 1, 1, 1 ], 
-                [ 1, 1, 1, 1, 0, 0 ],
-                [ 0, 1, 1, 1, 0, 0 ],
-                [ 0, 1, 0, 0, 0, 0 ] ]
+               [ 1, 0, 0, 1, 1, 1 ], 
+               [ 1, 1, 1, 1, 1, 1 ], 
+               [ 1, 1, 1, 1, 0, 0 ],
+               [ 0, 1, 1, 1, 0, 0 ],
+               [ 0, 1, 0, 0, 0, 0 ] ]
 Ducked_dino_array= [ [ 0, 0, 0, 0, 0, 0 ],
-                [ 0, 0, 0, 0, 0, 0 ], 
-                [ 1, 1, 1, 1, 1, 1 ], 
-                [ 0, 1, 1, 1, 1, 1 ],
-                [ 0, 1, 1, 1, 0, 0 ],
-                [ 0, 1, 0, 0, 0, 0 ] ]
+                     [ 0, 0, 0, 0, 0, 0 ], 
+                     [ 1, 1, 1, 1, 1, 1 ], 
+                     [ 0, 1, 1, 1, 1, 1 ],
+                     [ 0, 1, 1, 1, 0, 0 ],
+                     [ 0, 1, 0, 0, 0, 0 ] ]
 ptera_array =  [ [ 0, 0, 0, 0 ],
-                   [ 0, 1, 0, 0 ], 
-                   [ 1, 1, 1, 1 ], 
-                   [ 0, 0, 0, 0 ] ]   
+                 [ 0, 1, 0, 0 ], 
+                 [ 1, 1, 1, 1 ], 
+                 [ 0, 0, 0, 0 ] ]   
 Cactus_array = [ [ 0, 0, 1, 0 ],
-                   [ 0, 1, 1, 1 ], 
-                   [ 0, 0, 1, 0 ], 
-                   [ 0, 0, 1, 0 ] ]      
+                 [ 0, 1, 1, 1 ], 
+                 [ 0, 0, 1, 0 ], 
+                 [ 0, 0, 1, 0 ] ]      
+
+background = [[0 for x in range(32)] for x in range(16)]
+
+def LED_init():
+    t=threading.Thread(target=led.main, args=())
+    t.setDaemon(True)
+    t.start()
+    return
 
 class Dino():
     def __init__(self, x, y):
@@ -61,6 +71,7 @@ class Dino():
                 for j in range(6):
                     if dino_array[i][j] == 1:
                         pygame.draw.rect(win, WHITE, [int(self.x+j*Pixel), int(self.y+i*Pixel-5*Pixel), Pixel, Pixel])
+                        led.set_pixel(int((self.x+j*Pixel)/Pixel), int((self.y+i*Pixel-5*Pixel)/Pixel), 3)
             self.Col_U_D = pygame.Rect(int(self.x+0*Pixel), int(self.y-5*Pixel), 6*Pixel, 3*Pixel)
             self.Col_L_D = pygame.Rect([int(self.x+Pixel), int(self.y-2*Pixel), 3*Pixel, 2*Pixel])
                         
@@ -69,6 +80,7 @@ class Dino():
                 for j in range(6):
                     if Ducked_dino_array[i][j] == 1:
                         pygame.draw.rect(win, WHITE, [int(self.x+j*Pixel), int(self.y+i*Pixel-5*Pixel), Pixel, Pixel])
+                        led.set_pixel(int((self.x+j*Pixel)/Pixel), int((self.y+i*Pixel-5*Pixel)/Pixel), 3)
             self.Col_U_D = pygame.Rect(int(self.x+0*Pixel), int(self.y-3*Pixel), 6*Pixel, 2*Pixel)
             self.Col_L_D = pygame.Rect([int(self.x+1*Pixel), int(self.y-Pixel), 3*Pixel, 2*Pixel])
 
@@ -83,6 +95,7 @@ class Cactus():
             for j in range(4):
                 if Cactus_array[i][j] == 1:
                     pygame.draw.rect(win, GREEN, [int((self.Cacti_loc_x*Pixel - Pixel)+j*Pixel), int((self.Cacti_loc_y * Pixel - 4*Pixel)+i*Pixel), Pixel, Pixel])
+                    led.set_pixel(int(((self.Cacti_loc_x*Pixel - Pixel)+j*Pixel)/Pixel), int(((self.Cacti_loc_y*Pixel -*Pixel)+i*Pixel)/Pixel), 3)
         self.Col_C_X = pygame.Rect(int(self.Cacti_loc_x*Pixel - 0*Pixel), int(self.Cacti_loc_y * Pixel - 3*Pixel), 3*Pixel, Pixel)
         self.Col_C_Y = pygame.Rect(int(self.Cacti_loc_x*Pixel - (-1)*Pixel), int(self.Cacti_loc_y * Pixel - 4*Pixel), Pixel, 4*Pixel)
     def update(self):
@@ -94,18 +107,21 @@ class Box():
         self.Box_loc_x = 45
         self.Box_loc_y = 16
         self.Col_B = 0
+        self.rand = 1
         self.COLOR = RED
         self.disappear = False
     def draw(self):
         if self.disappear == False:
             pygame.draw.rect(win, self.COLOR, [int(self.Box_loc_x*Pixel - Pixel), int(self.Box_loc_y * Pixel - 10*Pixel), Pixel, Pixel])
+            led.set_pixel(int((self.Box_loc_x*Pixel - Pixel)/Pixel), int((self.Box_loc_y*Pixel - 10*Pixel)/Pixel), self.rand)
         else:
             pass
         self.Col_B = pygame.Rect(int(self.Box_loc_x*Pixel - Pixel), int(self.Box_loc_y * Pixel - 10*Pixel), Pixel, Pixel)
     def update(self):
         self.Box_loc_x -= speed
         if int(self.Box_loc_x*Pixel - Pixel) <= 0:
-            self.COLOR = choice(Color_Set)
+            self.rand = randint(0, 7)
+            self.COLOR = Color_Set[self.rand]
             self.Box_loc_x = randint(32, 100)
             self.disappear = False
 
@@ -120,6 +136,7 @@ class Ptera():
             for j in range(4):
                 if ptera_array[i][j] == 1:
                     pygame.draw.rect(win, YELLOW, [int((self.Ptera_loc_x*Pixel - Pixel)+j*Pixel), int((self.Ptera_loc_y * Pixel - 7*Pixel)+i*Pixel), Pixel, Pixel])
+                    led.set_pixel(int(((self.Ptera_loc_x*Pixel - Pixel)+j*Pixel)/Pixel), int(((self.Ptera_loc_y*Pixel - 7*Pixel)+i*Pixel)/Pixel), 4)
         self.Col_P = pygame.Rect(int(self.Ptera_loc_x*Pixel - Pixel), int((self.Ptera_loc_y * Pixel - 6*Pixel)+0*Pixel), 4*Pixel, 2*Pixel)
         #pygame.draw.rect(win, ORANGE, [int(self.Ptera_loc_x*Pixel - Pixel), int(self.Ptera_loc_y * Pixel - 4*Pixel), Pixel, Pixel])
     def update(self):
@@ -128,7 +145,16 @@ class Ptera():
             self.Ptera_loc_x = randint(32, 100)
             self.Ptera_loc_y = randint(13, 16)
 
+class Background():
+    def draw(self):
+        for i in range(16):
+            for j in range(32):
+                if background[i][j] == 0:
+                    led.set_pixel(j, i, 0)
 
+LED_init()
+
+S = Background()
 D = Dino(X, Y)            
 C = Cactus()
 B = Box()
@@ -136,6 +162,7 @@ P = Ptera()
 
 while run:
     win.fill(BLACK)
+    S.draw()
     D.draw()
     C.draw()
     B.draw()
